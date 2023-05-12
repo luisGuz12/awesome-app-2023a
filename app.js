62701287ae6a96496f7ef49694b21fedfecbde36
guzman jimenez luis alfonso <124893198+luisGuz12@users.noreply.github.com>
2023-05-12 15:42:52 +0000

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import http from 'http';
+//import http from 'http';
 
 //importando expressjs
 import express from 'express';
@@ -29,14 +29,14 @@ app.use((req, res) => {
 });
 
 //creando servidor 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
 //definir puertos 
 const port = 3000;
 const ip = "0.0.0.0";
 
 //arrancando el server
-server.listen(port, ip, (err)=>{ 
+app.listen(port, ip, (err)=>{ 
     console.log(`📣 sirviendo en ${process.env.ip}:3000`);
     console.log(`📣 sirviendo en ${process.env.port}:3000`);
  })
